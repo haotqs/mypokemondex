661f548ec5029fc603cb3405c0441a9f0beeb50e
haotqs <hao.tranquangsong@gmail.com>
2022-11-18 16:00:04 +0700

    update UI component

--- app/src/main/java/com/monastery/pokemondex/base/BaseActivity.kt
+++ app/src/main/java/com/monastery/pokemondex/base/BaseActivity.kt
@@ -2,13 +2,14 @@ package com.monastery.pokemondex.base
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import com.monastery.pokemondex.R
 import java.lang.Exception
 
-abstract class BaseActivity(@LayoutRes private val rootLayout: Int?): AppCompatActivity() {
+abstract class BaseActivity(@LayoutRes private val rootLayout: Int?): ComponentActivity() {
     lateinit var mFragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,34 +18,5 @@ abstract class BaseActivity(@LayoutRes private val rootLayout: Int?): AppCompatA
             setContentView(v)
         }
 
-        mFragmentManager = supportFragmentManager
-    }
-
-    fun replaceFragment(fragment: BaseFragment, isClearBackStack: Boolean = false) {
-        if (isClearBackStack) {
-            resetBackStack(fragmentManager = mFragmentManager)
-        }
-
-        onReplaceFragment(fragment)
-    }
-
-
-    private fun onReplaceFragment(fragment: BaseFragment) {
-        val mFragmentTAG = fragment.javaClass.simpleName
-
-        val transaction = mFragmentManager.beginTransaction()
-        transaction.addToBackStack(mFragmentTAG)
-            .replace(R.id.fragmentContainer, fragment, mFragmentTAG)
-            .commitAllowingStateLoss()
-    }
-
-    private fun resetBackStack(fragmentManager: FragmentManager) {
-        try {
-            while (fragmentManager.backStackEntryCount > 0) {
-                fragmentManager.popBackStackImmediate()
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
     }
 }
